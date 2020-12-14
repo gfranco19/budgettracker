ce51ba90efbdf7dd0768927649ad2637c70e4b09
Giovanny Franco <gfranco19@live.com>
2020-12-14 00:02:02 -0800

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// const PORT = 3000;
 
 const app = express();
 
@@ -29,6 +29,6 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
 });
